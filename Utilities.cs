5baed4b04103178a70bb05cccedef5bd229c1d20
Labradorite <35613869+Labradorite1@users.noreply.github.com>
2019-01-26 18:21:53 +0100

    Create Utilities.cs

---
+++ Utilities.cs
@@ -0,0 +1,42 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using Newtonsoft.Json;
+
+namespace AnthemRankBot
+{
+    class Utilities
+    {
+        private static Dictionary<string, string> alerts;
+
+        static Utilities()
+        {
+            string json = File.ReadAllText("Configuration/Emotes.json");
+            var data = JsonConvert.DeserializeObject<dynamic>(json);
+            alerts = data.ToObject<Dictionary<string, string>>();   
+        }
+
+        public static string GetAlert(string key)
+        {
+            if (alerts.ContainsKey(key)) return alerts[key];
+            return "";
+        }
+
+        public static string GetFormattedAlert(string key, params object[] parameter)
+        {
+            if (alerts.ContainsKey(key))
+            {
+                return String.Format(alerts[key], parameter);
+            }
+            return "JSON key not found.";
+        }
+
+        public static string GetFormattedAlert(string key, object parameter)
+        {
+            return GetFormattedAlert(key, new object[] { parameter });
+        }
+    }
+}
